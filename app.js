45f49baf8a7d28610a6467fcfc6887fe29a901aa
Leandro Farias <leandro.da.cruz.farias@gmail.com>
2020-02-16 13:10:17 -0400

    Express Session Part 1.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,11 +21,11 @@ connect.then((db) => {
 });
 
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
+    if (!req.session.user) {
         var authHeader = req.headers.authorization;
         if (!authHeader) {
             var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');              
+            res.setHeader('WWW-Authenticate', 'Basic');                        
             err.status = 401;
             next(err);
             return;
@@ -34,18 +35,20 @@ function auth(req, res, next) {
         var user = auth[0];
         var pass = auth[1];
         if (user == 'admin' && pass == 'password') {
-            res.cookie('user', 'admin', { signed: true });
+            req.session.user = 'admin';
             next(); // authorized
         } else {
             var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');              
+            res.setHeader('WWW-Authenticate', 'Basic');
             err.status = 401;
             next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
+            console.log('req.session: ', req.session);
             next();
-        } else {
+        }
+        else {
             var err = new Error('You are not authenticated!');
             err.status = 401;
             next(err);
@@ -62,7 +65,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
