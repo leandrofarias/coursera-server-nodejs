ef565d4a73228ebb6acf473fa5984b762e5129f6
Leandro Farias <leandro.da.cruz.farias@gmail.com>
2020-02-16 20:29:11 -0400

    Passport.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
 
+var authenticate = require('./authenticate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -21,18 +23,12 @@ connect.then((db) => {
 });
 
 function auth(req, res, next) {
-    if(!req.session.user) {
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
         err.status = 403;
-        return next(err);
+        next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
@@ -53,8 +49,13 @@ app.use(session({
     resave: false,
     store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
