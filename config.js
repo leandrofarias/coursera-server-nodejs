ecaff381d532012bd54b8b08fce025999339a829
Leandro Farias <leandro.da.cruz.farias@gmail.com>
2020-02-20 22:04:11 -0400

    Passport Facebook.

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/confusion'
+    'mongoUrl' : 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '272803140359171',
+        clientSecret: '52e10e93129d6318562412b5073b210c'
+    }
 }
